edca0234f8219b176072fae88cbd990847f653fd
fikerte <tofikertesfaye@gmail.com>
2025-06-09 22:42:03 +0300

    Use Context API to share state across components

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,25 @@ import { useState } from 'react'
 import './App.css'
 import Calculator from './Components/Calculator'
 import Result from './Components/Result'
+import { FormContext } from './Contexts/FormContext'
 
 function App() {
-  const [message, setMessage] = useState({})
+
+  const [formValues, setFormValues] = useState({})
+  const [displayData, setDisplayData] = useState({})
+  const [formTarget, setFormTarget] = useState(null)
+  const [formErrors, setFormErrors] = useState({})
   
-  const handleDataFromCalculator = (calcData) => {
-    setMessage(calcData)
-  }
   return (
     <>
-      <main className='font-plusjakarta bg-neutral-Slate100 min-h-screen font-primary flex justify-center items-center'>
-        <div className='flex flex-col w-lg md:flex-row md:justify-center md:w-4xl md:items-stretch bg-neutral-White md:rounded-2xl'>
-          <Calculator onDataSubmit={handleDataFromCalculator}/>
-          <Result message={message} />
-        </div>
-          
-      </main>
+      <FormContext.Provider value={{formValues, displayData, formTarget, formErrors, setFormValues, setDisplayData, setFormTarget, setFormErrors }}>
+        <main className='font-plusjakarta bg-neutral-Slate100 min-h-screen font-primary flex justify-center items-center'>
+          <div className='flex flex-col w-lg md:flex-row md:justify-center md:w-4xl md:items-stretch bg-neutral-White md:rounded-2xl'>
+            <Calculator />
+            <Result />
+          </div>  
+        </main>
+      </FormContext.Provider>
     </>
   )
 }
--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { FormContext } from '../Contexts/FormContext'
 
-const Result = ({message}) => {
+const Result = () => {
+  const {displayData} = useContext(FormContext)
+
   const formatNumber = (num) => {
     if(isNaN(num)) return '0.00'
     return Number(num).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})
   }
+  
   return (
       <div className='bg-neutral-Slate900/95 md:w-1/2 flex md:rounded-lg md:rounded-bl-[4rem]'>
       {
-        (Object.keys(message).length > 0) ? 
+        (Object.keys(displayData).length > 0) ? 
         (
           <div className='flex flex-col items-baseline p-8 space-y-4 '>
             <h2 className='font-bold text-neutral-White text-2xl'>Your results</h2>
@@ -17,11 +21,11 @@ const Result = ({message}) => {
             <div className='bg-neutral-Slate900 rounded-lg w-full mt-6 p-8 border-t-4  border-t-primary-lime'>
               <div className='pb-8 border-b-1 border-b-neutral-Slate700'>
                 <p className='text-neutral-Slate300 text-sm mb-2'>Your monthly repayments</p>
-                <p className='text-primary-lime font-bold text-6xl'>{message.type === 'repayment' ? formatNumber(message.monthlyRepay) || '0.00' : message.type === 'interest' ? formatNumber(message.monthlyInterest) : '0.00'}</p>
+                <p className='text-primary-lime font-bold text-6xl'>{displayData.type === 'repayment' ? formatNumber(displayData.monthlyRepay) || '0.00' : displayData.type === 'interest' ? formatNumber(displayData.monthlyInterest) : '0.00'}</p>
               </div>
               <div className='pt-8'>
                 <p className='text-neutral-Slate300 text-sm mb-2 font-medium'> Total you'll repay over the term</p>
-                <p className='text-neutral-White text-2xl'>{message.type === 'repayment' ? formatNumber(message.totalRepay) || '0.00' : message.type === 'interest' ? formatNumber(message.totalInterest) : '0.00'}</p>
+                <p className='text-neutral-White text-2xl'>{displayData.type === 'repayment' ? formatNumber(displayData.totalRepay) || '0.00' : displayData.type === 'interest' ? formatNumber(displayData.totalInterest) : '0.00'}</p>
               </div>
             </div>
           </div> 
